2d4aa3bc3fef9700e3cd17f8ac9a371925448dcb
Cole Blackman <coleblax@gmail.com>
2023-02-15 13:46:26 -0500

    Add files via upload

---
+++ main.java
@@ -0,0 +1,18 @@
+package passwordGenerator;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("Enter your ")
+		
+		// Requirement 1: Specify number of special characters, letters, capital letters, numbers, password length
+		// Requirement 2: Be able to store and come back to previously generated passwords?
+		// Requirement 3: Hash passwords when storing them?
+		// Requirement 4: Ensure that passwords are pronouncable?
+		// Requirement 5: Give a strength estimate of the generated password
+			// Requirement 5a: Specify number of days/months/years an average computer would require to break the password by brute force
+			// Requirement 5b: Test password strength against JTR, RT, etc.
+		
+	}
+
+}
