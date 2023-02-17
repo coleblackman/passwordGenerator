ec7c9577e56297aa9f334529d8fd96d8ee2c31b7
Cole Blackman <coleblax@gmail.com>
2023-02-16 20:17:03 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,12 +1,45 @@
 package passwordGenerator;
+import javax.swing.*;
 
+import java.awt.Font;
+import java.awt.event.*;
 import java.security.SecureRandom;
 
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to password generator");
-		System.out.println("A random char is: " + alphaGen());
+		int passwordLength = 10;
+
+		JFrame frame = new JFrame("Password Generator");
+	    JButton button = new JButton("Generate");
+	    JTextField password = new JTextField();
+	    password.setBounds(125, 50, 150, 30); // set the size and position of the text field
+	      
+	    button.setFont(new Font("Arial", Font.PLAIN, 7)); // set the font of the button
+
+	    button.addActionListener(new ActionListener() {
+	    	public void actionPerformed(ActionEvent e) {
+	    		password.setText(newPasswordGen(passwordLength));
+	    	}
+	    });
+
+	    button.setBounds(150, 200, 100, 30); // set the size and position of the button
+	    frame.setLayout(null);
+	    frame.add(password);
+	    frame.add(button);
+	    frame.setSize(400, 300);
+
+	    frame.setVisible(true);
+	    
+	    
+		
+	
+		
+		
+		for(int i = 0; i < passwordLength; i++)
+		{
+			
+		}
 		
 		// Requirement 1: Specify number of special characters, letters, capital letters, numbers, password length
 		// Requirement 2: Hash passwords when storing them?
@@ -18,6 +51,16 @@ public class main {
 		// Requirement 6: Be able to store and come back to previously generated passwords?
 
 		
+	}
+	
+	public static String newPasswordGen (int length) {
+		String pass = "";
+		for(int i = 0; i < length; i++)
+		{
+			pass = pass + alphaGen();
+		}
+		
+		return pass;
 	}
 	
 	public static char alphaGen () {
